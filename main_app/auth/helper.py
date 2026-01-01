bbb8140be4ef3d4e9e599e2c25ed23345da29d87
JohnStares <wisdom8achor24@gmail.com>
2026-01-01 10:06:36 +0100

    Added:
    - send email function to send email used for password reset
    - save token function to add tokens generated to that database
    - validate token function to validate the token during reset-password

--- main_app/auth/helper.py
+++ main_app/auth/helper.py
@@ -1,15 +1,17 @@
 from flask_mail import Message
-from main_app.extensions import mail
+from main_app.extensions import mail, db
 from secrets import token_hex
+from main_app.models import ResetToken
+import sqlalchemy as sql
+from datetime import datetime, timezone, timedelta
+from flask import Flask
+from typing import Literal
 
-TOKEN: list[str] = []
-
-def send_email(user_id: int, receiver: str) -> None:
-    token = password_reset_token(user_id)
+def send_email(receiver: str, token: str, host_url: str) -> None:
     msg = Message(
         subject="Password Reset",
         recipients=[receiver],
-        body=f"Here is your password reset token\n{token}\nNote: Token invalidates after 15 mins."
+        body=f"Here is your password reset link\n{host_url}/auth/reset-password/{token}\nNote: Link invalidates after 10 mins."
     )
 
     try:
@@ -18,21 +20,42 @@ def send_email(user_id: int, receiver: str) -> None:
         raise
 
 
-def password_reset_token(user_id: int) -> str:
+def save_token(user_id: int, token: str, current_app: Flask) -> None:
     if not isinstance(user_id, int):
-        raise TypeError(f"Expects an int but got {type(user_id)}")
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            raise
 
-    token =  f"{user_id}_{token_hex(16)}"
-    TOKEN.append(token)
-
-    return token
-
-
-def validate_token(token: str) -> bool:
     if not isinstance(token, str):
-        raise ValueError(f"Token is expected to be a string but got {type(token)}")
+        try:
+            token =  str(token)
+        except ValueError:
+            raise
     
-    if token in TOKEN:
-        return True
+    expires_at = datetime.now(timezone.utc) + timedelta(minutes=10)
+
+    try:
+        new_token = ResetToken(user_id=user_id, token=token, expires_at=expires_at)
+        db.session.add(new_token)
+        db.session.commit()
+        current_app.logger.info(f"New token saved to {user_id} with a token {token}")
+
+    except Exception as e:
+        current_app.logger.error(f"AN error occured while saving token {token} to user {user_id} due to {str(e)}", exc_info=True)
+        raise
+
+
+def validate_token(token: str) -> (tuple[Literal[True], int] | tuple[Literal[False], None]):
+    if not isinstance(token, str):
+        try:
+            token = str(token)
+        except ValueError:
+            raise
+
+    token_obj = db.session.scalar(sql.select(ResetToken).where(ResetToken.token == token))
+
+    if token_obj and token_obj.is_valid():
+        return (True, token_obj.user_id)
     
-    return False
+    return (False, None)
