dab844c5bb96881f34ef09acb189f280541921eb
JohnStares <wisdom8achor24@gmail.com>
2026-01-15 11:29:04 +0100

    Registered all blueprints in another file and call it in the init file to reduce excess code from the init file

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -29,16 +29,11 @@ def create_app(config_name: Optional[str] = None):
     register_handlers(app)
 
     # Register blueprints
-    from main_app.auth import auth_bp
-    from main_app.main import main_bp
-    from main_app.admin import admin_bp
-
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(main_bp)
-    app.register_blueprint(admin_bp, url_prefix="/admin")
+    register_blueprints(app)
 
     @app.cli.command("create-db")
     def create_db():
+        from main_app.extensions import db
         """Create database tables for development only"""
         print("Creating database tables...")
         db.create_all()
